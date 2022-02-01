aee1b4a08a8f113d7fe9620e34514e5bb254bd7f
Lmaxplay <lucamollema@outlook.com>
2022-02-01 13:57:51 +0100

    UPDATESSSSSSSSSS

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <filesystem>
-#include <windows.h>
-#include <direct.h>
-#include <fstream>
-#include <string>
-#include <limits>
+#include <iostream> // Allow IO
+#include <filesystem> // Filesystem access
+#include <windows.h> // Windows API
+#include <direct.h> // Direct API
+#include <fstream> // Filestream
+#include <string> // string related functions
+#include <limits> // Limits
 
 #include "lib/getExecutableName.h" // Currently unused
 #include "lib/vector.hpp"
@@ -16,8 +16,6 @@ int main() {
     vec4ld a = {10.0, 20.0, 30.0};
     vec4ld b = {10.0, 20.0, 30.0};
     vec4ld c = a + b;
-    c[2];
     cout << "Of the vector (" << c << ") the length is: " << c.getmagnitude() << " its size is: " << sizeof(c) << "\n";
     return 0;
 }
-//imma code in vector.hpp
