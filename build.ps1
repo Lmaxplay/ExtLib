14b0de71d707e6bf801a6658dcbc24918f7be16c
Lmaxplay <lucamollema@outlook.com>
2022-02-05 18:04:27 +0100

    UPDATES!!!

--- build.ps1
+++ build.ps1
@@ -6,6 +6,23 @@ param(
     [Parameter(Mandatory=$False)][System.String]$IncludePath = "C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/um"
 )
 
+$PSMinVersion = 7
+$PSVersion = $PSVersionTable.PSVersion.Major
+if($PSVersionTable.PSVersion.Major -lt 6) {
+    $PSVersionFull = $PSVersionTable.PSVersion.Major.ToString() + "." + $PSVersionTable.PSVersion.Minor.ToString() + "." + $PSVersionTable.PSVersion.Revision.ToString()
+} else {
+    $PSVersionFull = $PSVersionTable.PSVersion.Major.ToString() + "." + $PSVersionTable.PSVersion.Minor.ToString() + "." + $PSVersionTable.PSVersion.Patch.ToString()
+}
+
+$OS = "Unknown OS"
+if ($IsWindows) {
+    $OS = "Windows"
+} elseif ($IsMacOS) {
+    $OS = "Mac OS"
+} elseif ($IsLinux) {
+    $OS = "Linux" + $Env:OS
+}
+
 # MSVC is C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.30.30705/bin/Hostx64/x64/cl
 # Clang is C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang++.exe
 
@@ -67,7 +84,14 @@ function Write-Blue {
 }
 
 try {
-    Write-Green 'Lmaxplay CPP build script v1.1.5' 'Licensed under the MIT License' 'Copyright 2022 Lmaxplay'
+    if ($PSVersion -lt 7) {
+        Write-Red "This version of powershell is not supported, please use Powershell version $PSMinVersion or higher" "You are using Powershell version $PSVersionFull"
+        exit
+    }
+
+    Write-Green 'Lmaxplay CPP build script v1.1.5' 'Licensed under the MIT License' 'Copyright 2022 Lmaxplay' ""
+
+    #Write-Cyan "Running on PowerShell version $PSVersionFull" ""
 
     $OOption = "-O$O"
 
@@ -76,17 +100,29 @@ try {
     Write-Cyan 'Running compiler...'
 
     $CompilerTimer = [Diagnostics.Stopwatch]::StartNew()
-    if ($Compiler -eq 0) {
-        Write-Blue "Using Clang"
-        C:/"Program Files"/"Microsoft Visual Studio"/"2022"/Community/VC/Tools/Llvm/x64/bin/clang++.exe -masm=intel -std=c++20 $OOption $WallOption -I$IncludePath -o 'output/app.exe' 'src/*.cpp' 'src/lib/*.cpp' -g
-    } elseif ($Compiler -eq 1) {
-        Write-Blue "Using GCC"
-        g++ -std=c++20 $OOption $WallOption -I$IncludePath -o'output/app.exe' 'src/**.cpp' 'src/lib/*.cpp' -g
-    } elseif ($Compiler -eq 2) {
-        Write-Blue "Using MSVC"
-        C:/"Program Files"/"Microsoft Visual Studio"/"2022"/Community/VC/Tools/MSVC/14.30.30705/bin/Hostx64/x64/cl -Zi -DDEBUG -Fe:'output/app.exe' -std:c++20 $OOption $WallOption -nologo -I"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.30.30705\include" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/ucrt" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/um" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/winrt" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/shared" 'src/*.cpp' 'src/lib/*.cpp' -EHsc -Fo:"output/" -Fd:"output/app.output.pdb" /link -LIBPATH:'C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.30.30705\lib\x64' -LIBPATH:'C:\Program Files\Microsoft Visual Studio\2022\Community\SDK\ScopeCppSDK\vc15\SDK\lib'
+    if ($IsWindows) {
+        if ($Compiler -eq 0) {
+            Write-Blue "Using Clang"
+            C:/"Program Files"/"Microsoft Visual Studio"/"2022"/Community/VC/Tools/Llvm/x64/bin/clang++.exe -masm=intel -std=c++20 $OOption $WallOption -I$IncludePath -o 'output/app.exe' 'src/*.cpp' 'src/lib/*.cpp' -g
+        } elseif ($Compiler -eq 1) {
+            Write-Blue "Using GCC on Windows"
+            g++ -std=c++20 $OOption $WallOption -I$IncludePath -o'output/app.exe' 'src/**.cpp' 'src/lib/*.cpp' -g
+        } elseif ($Compiler -eq 2) {
+            Write-Blue "Using MSVC"
+            C:/"Program Files"/"Microsoft Visual Studio"/"2022"/Community/VC/Tools/MSVC/14.30.30705/bin/Hostx64/x64/cl -Zi -DDEBUG -Fe:'output/app.exe' -std:c++20 $OOption $WallOption -nologo -I"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.30.30705\include" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/ucrt" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/um" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/winrt" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.22000.0/shared" 'src/*.cpp' 'src/lib/*.cpp' -EHsc -Fo:"output/" -Fd:"output/app.output.pdb" /link -LIBPATH:'C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.30.30705\lib\x64' -LIBPATH:'C:\Program Files\Microsoft Visual Studio\2022\Community\SDK\ScopeCppSDK\vc15\SDK\lib'
+        } else {
+            Write-Blue "Invalid compiler, Compiler number $Compiler is not configured"
+        }
+    } elseif ($IsLinux) {
+        if ($True) {
+            Write-Blue "Using GCC on Linux"
+            g++ -std=c++20 $OOption $WallOption -o 'output/app' 'src/main.cpp' -g
+        }
+    } elseif ($IsMacOS) {
+        Write-Red "Mac OS is not supported"
+        exit
     } else {
-        Write-Blue "Invalid compiler, Compiler number $Compiler is not configured"
+        Write-Red "Could not determine OS, thus no compilation executed"
     }
     $CompilerTimer.Stop()
     $CompileTime = $CompilerTimer.Elapsed
