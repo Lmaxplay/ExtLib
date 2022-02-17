55d39b5468502a5bc015e2098cfcceffb498d32a
Lmaxplay <lmaxplay@outlook.com>
2022-02-17 17:18:54 +0100

    Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -27,34 +27,14 @@
 
 #include "lib/betterwindows.hpp"
 
-
 void clearconsole() {
     consolereset;
 }
 
+
 // Main function
 int main() {
     std::atexit(clearconsole);
-    std::cout << "Hello World\n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "Hello worl \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "Hello wor  \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "Hello wo   \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "Hello w    \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "Hello      \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "Hell       \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "Hel        \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "He         \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "H          \n";
-    sleeps::miliseconds(1000);
-    std::cout << ansi::upline << "           \n";
+    std::cout << 1.1 * 1.1 << "\n";
     return 0;
 }
