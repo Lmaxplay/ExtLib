2bb89c94c99b41abfe03b17cbb2917a6c2655a0a
Lmaxplay <lmaxplay@outlook.com>
2022-03-30 13:06:45 +0200

    name mistake and microseconds!

--- src/lib/sleep.cpp
+++ src/lib/sleep.cpp
@@ -6,12 +6,16 @@ namespace sleeps {
         std::this_thread::sleep_for(std::chrono::seconds(s));
     }
 
-    void miliseconds(long long ms) {
+    void milliseconds(long long ms) {
         std::this_thread::sleep_for(std::chrono::milliseconds(ms));
     }
 
     void nanoseconds(long long ns) {
         std::this_thread::sleep_for(std::chrono::nanoseconds(ns));
+    }
+
+    void microseconds(long long us) {
+        std::this_thread::sleep_for(std::chrono::microseconds(us));
     }
 
 }
--- src/lib/sleep.hpp
+++ src/lib/sleep.hpp
@@ -5,8 +5,10 @@ namespace sleeps {
 
     void seconds(long long s);
 
-    void miliseconds(long long ms);
+    void milliseconds(long long ms);
 
     void nanoseconds(long long ns);
+
+    void microseconds(long long us);
 
 }
